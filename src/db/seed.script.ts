be55fb38b595a6807084b3b1d230db5f3ad533a9
Nick Olson <naolson.1819@gmail.com>
2025-06-13 09:01:32 -0500

    wip seed script now creates realistic users

--- src/db/seed.script.ts
+++ src/db/seed.script.ts
@@ -1,11 +1,18 @@
 import * as schema from './schema';
 import { seed } from 'drizzle-seed';
-import { db } from './db';
+import { db, pool } from './db';
 
 const seedDb = async () => {
-  await seed(db, schema).refine(() => ({
+  await seed(db, schema).refine((funcs) => ({
     usersTable: {
-      columns: {},
+      columns: {
+        age: funcs.int({ minValue: 0, maxValue: 120 }),
+        // This is "password123" hashed with argon2
+        passwordHash: funcs.default({
+          defaultValue:
+            '$argon2id$v=19$m=65536,t=2,p=1$fiKHnn0LRcOmaHJ21amaIHdCMOJPIgQgWu6l5az9dTo$6PG1lcOkeV+yjD2mW8eBfmvZX6z1fDt1wmyiGrwZSng',
+        }),
+      },
       count: 10,
       with: {
         todosTable: 10,
@@ -15,5 +22,11 @@ const seedDb = async () => {
 };
 
 seedDb()
-  .then(() => console.log('Database seeded successfully'))
-  .catch((err) => console.error(`Error seeding database: ${err}`));
+  .then(() => {
+    console.log('Database seeded successfully');
+    return pool.end();
+  })
+  .catch((err) => {
+    console.error('Error seeding database:', err);
+    return pool.end();
+  });
